9aeb0ed2485c126b2b2b36e492db5f8ac114ec43
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-08-12 17:23:56 +1000

    created seeds.sql to add data for each section

---
+++ seeds.sql
@@ -0,0 +1,29 @@
+-- Add sample department data
+INSERT INTO Department (name)
+VALUES
+('HR'),
+('Finance'),
+('Sales'),
+('Engineering'),
+('Tech'),
+('Legal');
+
+-- Add sample role data
+INSERT INTO Role (title, salary, department_id)
+VALUES
+('Manager', 70000, 1),
+('Accountant', 95000, 2),
+('Sales Lead', 85000, 3),
+('Engineer', 90000, 4),
+('Software Engineer', 105000, 5),
+('Barrister', 110000, 6);
+
+-- Add sample employee data
+INSERT INTO Employee (first_name, last_name, role_id, manager_id)
+VALUES
+('John', 'Doe', 1, 1),
+('Michael', 'Johnson', 3, 1);
+('Alec', 'Baldwin', 4, 3),
+('Tom', 'Cruise', 5, 1);
+('Al', 'Pacino', 6, 3),
+('Jean', 'Claude', 3, 1);
